0af789e3feecbcbb4a8b5fa6e8ec19a6ded3c96a
sivalingamsundararajshanthi <lingams200@gmail.com>
2020-03-25 20:29:07 -0400

    Fix indentation

--- IsUnique.java
+++ IsUnique.java
@@ -1,27 +1,26 @@
 private boolean isUniqueBruteForce(String s) {
-
-        for (int i=0;i<s.toCharArray().length;i++) {
-            for (int j=i+1;j<s.toCharArray().length;j++) {
-                if(s.toCharArray()[i] == s.toCharArray()[j])
-                    return false;
+    for (int i=0;i<s.toCharArray().length;i++) {
+        for (int j=i+1;j<s.toCharArray().length;j++) {
+            if(s.toCharArray()[i] == s.toCharArray()[j])
+                return false;
             }
         }
 
-        return true;
-    }
+    return true;
+}
 
-    private boolean isUnique(String s) {
-        if (s.length() > 128) return false;
+private boolean isUnique(String s) {
+    if (s.length() > 128) return false;
+    
+    boolean[] bArray = new boolean[128];
+    
+    for (int i=0;i<s.length();i++) {
+        int val = s.charAt(i);
         
-        boolean[] bArray = new boolean[128];
+        if (bArray[val]) return false;
         
-        for (int i=0;i<s.length();i++) {
-            int val = s.charAt(i);
-            
-            if (bArray[val]) return false;
-            
-            bArray[val] = true;
-        }
-        
-        return true;
+        bArray[val] = true;
     }
+    
+    return true;
+}
